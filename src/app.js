1c29cc2bfe5de7c1a45281d54b255c983304d4eb
= <=>
2025-03-12 23:19:28 +0530

    feat: add healthcheck route and controller for API status verification

--- src/app.js
+++ src/app.js
@@ -21,9 +21,11 @@ app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
