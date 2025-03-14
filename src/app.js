3d397cdd0c181651ccaa10210b9b7a5603e648de
= <=>
2025-03-14 22:02:37 +0530

    feat: add like functionality for videos, comments, and tweets with corresponding routes

--- src/app.js
+++ src/app.js
@@ -23,11 +23,13 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/like", likeRouter);
 
 export { app };
