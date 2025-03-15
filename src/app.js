e22e48e0a61532a402e68136bf7af19fc3067215
= <=>
2025-03-15 17:31:23 +0530

    feat: add comment functionality with routes for fetching, adding, updating, and deleting comments on videos

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -33,5 +34,6 @@ app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/comments", commentRouter);
 
 export { app };
