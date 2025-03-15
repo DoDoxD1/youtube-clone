d007b038541c1f958b5d5c045782844756bee9f3
= <=>
2025-03-15 11:38:25 +0530

    feat: add tweet functionality with routes for creating, updating, deleting, and fetching user tweets

--- src/app.js
+++ src/app.js
@@ -24,12 +24,14 @@ import videoRouter from "./routes/video.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/playlist", playlistRouter);
-app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
